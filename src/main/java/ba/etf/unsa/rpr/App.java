a61b449bfb2a33efd4113b466678f7e672639293
mdur1c <mduric1@etf.unsa.ba>
2022-11-19 20:32:50 +0100

    1.5 updated pom.xml and gitignore

--- src/main/java/ba/etf/unsa/rpr/App.java
+++ src/main/java/ba/etf/unsa/rpr/App.java
@@ -5,6 +5,19 @@ public class App
 {
 
     public static void main(String[] args) {
+        String string = new String();
 
-}
+        for (int i=0; i< args.length; i++) {
+            if (i!=0)
+                string+=" ";
+
+            string+=args[i];
+        }
+
+        try {
+            System.out.println(ExpressionEvaluator.evaluate(string));
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
